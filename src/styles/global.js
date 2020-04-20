0054b3cc57905da41be60730cad119db7ff5f7b1
Rafael Muttoni <rmuttonic@gmail.com>
2020-04-20 19:45:01 -0300

    Added background and font to global styles

--- src/styles/global.js
+++ src/styles/global.js
@@ -47,7 +47,9 @@ export default createGlobalStyle`
   }
 
   body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     min-height: 100%;
     -webkit-font-smoothing: antialiased !important;
+    background: #fbd743;
   }
 `;
