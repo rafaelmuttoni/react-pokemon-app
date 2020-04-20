b7fcaba98c8744355584fc03cc90e2bcd7fa25e8
Rafael Muttoni <rmuttonic@gmail.com>
2020-04-20 15:28:48 -0300

    Removed contact page

--- src/pages/Contact/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const ContactPage = () => <h1>Contact</h1>;
-
-export default ContactPage;
--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,12 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import MainPage from './pages/Main';
-import ContactPage from './pages/Contact';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={MainPage} />
-        <Route path="/contact" component={ContactPage} />
       </Switch>
     </BrowserRouter>
   );
