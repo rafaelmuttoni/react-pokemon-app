1b856e7d8036041d2887b7e02f78ec884761e881
Rafael Muttoni <rmuttonic@gmail.com>
2020-04-20 14:21:05 -0300

    Basic routing with 2 pages finished

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import MainPage from './pages/Main'
+import ContactPage from './pages/Contact'
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={MainPage} />
+        <Route path="/contact" component={ContactPage} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
